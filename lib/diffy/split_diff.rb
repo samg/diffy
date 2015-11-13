d21c6a3ed0ad3d40759876096614b28f11437b8e
Runar Skaare Tveiten <runar@tveiten.io>
2015-11-14 00:26:23 +0100

    Add support for side-by-side comparisons (split view)
    This commit introduces a new class; `Diffy::SplitDiff`. This class takes
    a diff returned from `Diffy::Diff` and splits it in two parts (or two
    sides): left and right. The left side represents deletions while the
    right side represents insertions.
    
    The class is used as follows:
    
    ```
    split = Diffy::SplitDiff.new(left, right, options = {})
    ```
    
    Unlinke `Diffy::Diff`, `Diffy::SplitDiff` does not use `#to_s` to output
    the resulting diff. Instead, two self-explanatory methods are used to
    output the diff: `#left` and `#right`
    
    The optional options hash is passed along to the main `Diff::Diff`
    class, so all default options such as full diff output are supported.
    
    The output format may be changed by passing the format with the options
    hash:
    
    ```
    Diffy::SplitDiff.new(left, right, { format: :html }
    ```
    
    All default formats are supported, and non-default formats will raise an
    ArgumentError.
    
    If the default output format is set with `Diffy::Diff.default_format`,
    then that is the format used to output the split diff as well. If no
    format is defined, `Diffy::Diff.default_format` falls back to `:text`.

---
+++ lib/diffy/split_diff.rb
@@ -0,0 +1,48 @@
+module Diffy
+  class SplitDiff
+    def initialize(left, right, options = {})
+      @format = options[:format] || Diffy::Diff.default_format
+
+      unless Format.instance_methods.include?(@format)
+        fail ArgumentError, "Format #{format.inspect} is not a valid format"
+      end
+
+      @diff = Diffy::Diff.new(left, right, options).to_s(@format)
+      @left_diff, @right_diff = split
+    end
+
+    %w(left right).each do |direction|
+      define_method direction do
+        instance_variable_get("@#{direction}_diff")
+      end
+    end
+
+    private
+
+    def split
+      [split_left, split_right]
+    end
+
+    def split_left
+      case @format
+      when :color
+        @diff.gsub(/\033\[32m\+(.*)\033\[0m\n/, '')
+      when :html, :html_simple
+        @diff.gsub(%r{\s+<li class="ins"><ins>(.*)</ins></li>}, '')
+      when :text
+        @diff.gsub(/^\+(.*)\n/, '')
+      end
+    end
+
+    def split_right
+      case @format
+      when :color
+        @diff.gsub(/\033\[31m\-(.*)\033\[0m\n/, '')
+      when :html, :html_simple
+        @diff.gsub(%r{\s+<li class="del"><del>(.*)</del></li>}, '')
+      when :text
+        @diff.gsub(/^-(.*)\n/, '')
+      end
+    end
+  end
+end
--- spec/diffy_spec.rb
+++ spec/diffy_spec.rb
@@ -588,6 +588,73 @@ baz
   end
 end
 
+describe Diffy::SplitDiff do
+  before do
+    ::Diffy::Diff.default_options.merge!(diff: '-U 10000')
+  end
+
+  it "should fail with invalid format" do
+    expected_fail = expect do
+      Diffy::SplitDiff.new("lorem\n", "ipsum\n", { format: :fail })
+    end
+    expected_fail.to raise_error(ArgumentError)
+  end
+
+  describe "#left" do
+    it "should only highlight deletions" do
+      string1 = "lorem\nipsum\ndolor\nsit amet\n"
+      string2 = "lorem\nipsumdolor\nsit amet\n"
+      expect(Diffy::SplitDiff.new(string1, string2).left).to eq <<-TEXT
+ lorem
+-ipsum
+-dolor
+ sit amet
+      TEXT
+    end
+
+    it "should also format left diff as html" do
+      string1 = "lorem\nipsum\ndolor\nsit amet\n"
+      string2 = "lorem\nipsumdolor\nsit amet\n"
+      expect(Diffy::SplitDiff.new(string1, string2, { format: :html}).left).to eq <<-HTML
+<div class="diff">
+  <ul>
+    <li class="unchanged"><span>lorem</span></li>
+    <li class="del"><del>ipsum<strong></strong></del></li>
+    <li class="del"><del><strong></strong>dolor</del></li>
+    <li class="unchanged"><span>sit amet</span></li>
+  </ul>
+</div>
+      HTML
+    end
+  end
+
+  describe "#right" do
+    it "should only highlight insertions" do
+      string1 = "lorem\nipsum\ndolor\nsit amet\n"
+      string2 = "lorem\nipsumdolor\nsit amet\n"
+      expect(Diffy::SplitDiff.new(string1, string2).right).to eq <<-TEXT
+ lorem
++ipsumdolor
+ sit amet
+      TEXT
+    end
+
+    it "should also format right diff as html" do
+      string1 = "lorem\nipsum\ndolor\nsit amet\n"
+      string2 = "lorem\nipsumdolor\nsit amet\n"
+      expect(Diffy::SplitDiff.new(string1, string2, { format: :html}).right).to eq <<-HTML
+<div class="diff">
+  <ul>
+    <li class="unchanged"><span>lorem</span></li>
+    <li class="ins"><ins>ipsumdolor</ins></li>
+    <li class="unchanged"><span>sit amet</span></li>
+  </ul>
+</div>
+      HTML
+    end
+  end
+end
+
 describe 'Diffy::CSS' do
   it "should be some css" do
     expect(Diffy::CSS).to include 'diff{overflow:auto;}'
